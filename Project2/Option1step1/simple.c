2326ffce255a4d20fc6a4e85e0aa666271192a33
tzur.almog@gmail.com <tzur.almog@gmail.com>
2020-08-13 15:09:18 -0400

    trying new machine

--- Project2/Option1step1/simple.c
+++ Project2/Option1step1/simple.c
@@ -9,14 +9,31 @@
  * Copyright John Wiley & Sons - 2018
  */
 
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+
+#define  GOLDEN_RATIO_PRIME 3.1415
+extern HZ;
+
+int gcd(int x, int y){
+    int gcd, i;
+    for( i=1; i <= x && i <= y; ++i)
+    {
+        // Checks if i is factor of both integers
+        if(x%i==0 && y%i==0)
+            gcd = i;
+    }
+
+    return gcd;
+}
 
 /* This function is called when the module is loaded. */
 static int simple_init(void)
 {
        printk(KERN_INFO "Loading Module\n");
+       printk(KERN_INFO "%lu\n", GOLDEN_RATIO_PRIME);
 
        return 0;
 }
@@ -24,6 +41,7 @@ static int simple_init(void)
 /* This function is called when the module is removed. */
 static void simple_exit(void) {
 	printk(KERN_INFO "Removing Module\n");
+    printk(KERN_INFO "%d\n", HZ);
 }
 
 /* Macros for registering module entry and exit points. */
